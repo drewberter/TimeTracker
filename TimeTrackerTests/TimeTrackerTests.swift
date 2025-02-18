d76ee7c9844065010ecdf4007c80f16e7fe2ea0b
drewberter <50645917+drewberter@users.noreply.github.com>
2025-02-18 14:51:05 -0700

    Initial Commit

---
+++ TimeTrackerTests/TimeTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackerTests.swift
+//  TimeTrackerTests
+//
+//  Created by Drew on 2/18/25.
+//
+
+import Testing
+@testable import TimeTracker
+
+struct TimeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
